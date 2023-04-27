9ec47e030b496036b35521e1808ad2330cc6b771
Stephen James <78153905+ItsMeSteve88@users.noreply.github.com>
2023-04-27 09:48:07 +0100

    profiles screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,30 @@
+import { NextPageContext } from "next"
+import { signOut, getSession } from "next-auth/react"
+import useCurrentUser from '@/hooks/useCurrentUser';
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  }
+}
+
 export default function Home() {
+  const {data: user} = useCurrentUser()
   return (
     <>
-      <h1 className="text-2xl text-orange-400">Steveflix</h1>
+      <h1 className="text-4xl text-orange-400">Steveflix</h1>
+      <p className="text-orange-500">Logged in as : {user.name}</p>
+      <button onClick={() => signOut()} className='h-10 w-full bg-orange-400'>Log Out</button>
     </>
   )
 }
